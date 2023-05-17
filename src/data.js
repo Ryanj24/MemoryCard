6b8aea4d31c19b4407412d59b72026531f46426a
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-05-17 08:50:09 +0100

    Created initial data array and added assets

---
+++ src/data.js
@@ -0,0 +1,23 @@
+import AstonMartin from './assets/Aston-Martin.png'
+import Audi from './assets/Audi.png'
+import Bentley from './assets/Bentley.png'
+import BMW from './assets/BMW.png'
+import Ferrari from './assets/Ferrari.png'
+import Ford from './assets/Ford.png'
+import Lamborghini from './assets/Lamborghini.png'
+import Mercedes from './assets/Mercedes.png'
+import Porsche from './assets/Porsche.png'
+
+const data = [
+    {id: 1, brand: 'Aston Martin', img: AstonMartin},
+    {id: 2, brand: 'Audi', img: Audi},
+    {id: 3, brand: 'Bentley', img: Bentley},
+    {id: 4, brand: 'BMW', img: BMW},
+    {id: 5, brand: 'Ferrari', img: Ferrari},
+    {id: 6, brand: 'Ford', img: Ford},
+    {id: 7, brand: 'Lamborghini', img: Lamborghini},
+    {id: 8, brand: 'Mercedes', img: Mercedes},
+    {id: 9, brand: 'Porsche', img: Porsche},
+]
+
+export default data
