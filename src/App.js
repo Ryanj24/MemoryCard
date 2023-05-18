22616c857e51591bd9b516d891acf273e4739c3b
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-05-18 09:53:40 +0100

    Added initial logic for updating scores and resetting upon click of card already selected previously

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './index.css'
 import HeadBanner from './components/HeadBanner';
 import ScoreCard from './components/ScoreCard';
 import Selections from './components/Selections';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import data from './data';
 
 function App() {
@@ -19,24 +19,53 @@ function App() {
 
   function handleClick(e) {
 
+    // Get the id of the card selected by the user
     let divId = Number(e.target.id)
 
+    // Loop through each brand object in the data
     for (let i = 0; i < data.length; i++) {
+
+      // If the id of the card selected matches the ith object
       if (divId === data[i].id) {
+
+        // Check if the user has previously selected that card
         if (!checkPreviousState(data[i])) {
-          setSelectedCard(data[i])
+
+          // If not, add the selected card to the array of previously selected cards
+          //setSelectedCard(data[i])
+          //setScore(score => score + 1)
+          updateScores()
           setPreviousCards([...previousCards, data[i]])
+        } else {
+          setScore(0);
         }
       }
     }
   }
 
   function checkPreviousState(obj) {
+
+    // Loop through all previously selected cards
     for (let i = 0; i < previousCards.length; i++) {
+
+      // Check if the card selected by the user has been previously selected
       if (obj === previousCards[i]) {
-        return
+
+        // If so, return true from the function
+        return true
       }
     }
+  }
+
+  function updateScores() {
+
+    if (score <= highScore) {
+      setScore(score => score + 1);
+    } else {
+      setScore(score => score + 1)
+      setHighScore(highScore => score)
+    }
+
   }
 
   return (
--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -5,10 +5,10 @@ const ScoreCard = ({ score, setScore, highScore, setHighScore }) => {
   return (
     <div className='scores'>
         <div className='current-score'>
-            <p>Score: 0</p>
+            <p>Score: {score}</p>
         </div>
         <div className='high-score'>
-            <p>High Score: 3</p>
+            <p>High Score: {highScore}</p>
         </div>
     </div>
   )
