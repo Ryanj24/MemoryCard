b0ba11d345e44380af377119b0d8b11b48e0d3b4
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-05-19 09:57:14 +0100

    Added more car brands

--- src/data.js
+++ src/data.js
@@ -1,23 +1,35 @@
+import AlfaRomeo from './assets/Alfa-Romeo.png'
 import AstonMartin from './assets/Aston-Martin.png'
 import Audi from './assets/Audi.png'
 import Bentley from './assets/Bentley.png'
 import BMW from './assets/BMW.png'
+import Corvette from './assets/Corvette.png'
 import Ferrari from './assets/Ferrari.png'
 import Ford from './assets/Ford.png'
+import Jaguar from './assets/Jaguar.png'
 import Lamborghini from './assets/Lamborghini.png'
+import Maserati from './assets/Maserati.png'
+import McLaren from './assets/McLaren.png'
 import Mercedes from './assets/Mercedes.png'
 import Porsche from './assets/Porsche.png'
+import RollsRoyce from './assets/Rolls-Royce.png'
 
 const data = [
-    {id: 1, brand: 'Aston Martin', img: AstonMartin},
-    {id: 2, brand: 'Audi', img: Audi},
-    {id: 3, brand: 'Bentley', img: Bentley},
-    {id: 4, brand: 'BMW', img: BMW},
-    {id: 5, brand: 'Ferrari', img: Ferrari},
-    {id: 6, brand: 'Ford', img: Ford},
-    {id: 7, brand: 'Lamborghini', img: Lamborghini},
-    {id: 8, brand: 'Mercedes', img: Mercedes},
-    {id: 9, brand: 'Porsche', img: Porsche},
+    {id: 1, brand: 'Alfa Romeo', img: AlfaRomeo},
+    {id: 2, brand: 'Aston Martin', img: AstonMartin},
+    {id: 3, brand: 'Audi', img: Audi},
+    {id: 4, brand: 'Bentley', img: Bentley},
+    {id: 5, brand: 'BMW', img: BMW},
+    {id: 6, brand: 'Corvette', img: Corvette},
+    {id: 7, brand: 'Ferrari', img: Ferrari},
+    {id: 8, brand: 'Ford', img: Ford},
+    {id: 9, brand: 'Jaguar', img: Jaguar},
+    {id: 10, brand: 'Lamborghini', img: Lamborghini},
+    {id: 11, brand: 'Maserati', img: Maserati},
+    {id: 12, brand: 'McLaren', img: McLaren},
+    {id: 13, brand: 'Mercedes', img: Mercedes},
+    {id: 14, brand: 'Porsche', img: Porsche},
+    {id: 15, brand: 'Rolls Royce', img: RollsRoyce},
 ]
 
 export default data
